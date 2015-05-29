ba1617df266c5e0085a2dbc03ae657713b7a7386
Rich Shea <richcshea@gmail.com>
2015-05-28 22:09:54 -0600

    add edit account email

--- spec/features/user_can_edit_account_info_spec.rb
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -6,13 +6,26 @@ describe "user" do
     user = User.create(username: "user", email: "user@example.com", password: "password")
     user_login(user)
     click_on("My Account")
-    click_on("Edit Name")
+    click_on("Edit Account")
 
     fill_in "Username", with: "updated_name"
     click_on("Submit")
 
     expect(page).to have_content("updated_name")
   end
+
+  it "can edit email" do
+    user = User.create(username: "user", email: "user@example.com", password: "password")
+    user_login(user)
+    click_on("My Account")
+    click_on("Edit Account")
+
+    fill_in "Email", with: "updated_email@example.com"
+    click_on("Submit")
+
+    expect(page).to have_content("updated_email@example.com")
+  end
+
 end
 
 def user_login(user)
